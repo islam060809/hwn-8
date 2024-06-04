9879d2ba58a08d1798d51939eb1d62346a2f78b3
Islam <gojo060809@gmail.com>
2024-06-04 19:05:36 +0600

    added

--- g.js
+++ g.js
@@ -2,7 +2,11 @@ const rot = document.querySelector(".rot")
 const root = document.querySelector(".root")
 const rooot = document.querySelector(".rooot")
 const roooot = document.querySelector(".roooot")
+const sale = document.querySelector(".sale")
 const canteA = document.querySelectorAll(".cate-a")
+const no1 = document.querySelector(".worth")
+const no2 = document.querySelector(".new-year")
+const no3 = document.querySelector(".less")
 const h3 = document.querySelector(".sc-h3")
 const url2 = "https://fakestoreapi.com/products"
 const url = "https://fakestoreapi.com/products"
@@ -22,7 +26,7 @@ getPro()
 function showPro(di) {
     for (const da of di) {
         rot.innerHTML += `<div class="card">
-    <img class="imgPro" src="${da.image}">
+    <img onclick="getProById(${da.id})" class="imgPro" src="${da.image}">
     <h2 class="name">${da.title}</h2>
     <p class="card-cate">${da.category}</p>
     <p class="card-price">${da.price}$</p>
@@ -45,7 +49,7 @@ getPro2()
 function showPro2(di) {
     for (const da of di) {
         root.innerHTML += `<div class="card2">
-    <img class="" src="${da.image}">
+    <img onclick="getProById(${da.id})" class="" src="${da.image}">
     <h2 class="name2">${da.title}</h2>
     </div>
     `
@@ -70,7 +74,7 @@ function checkAdult(age) {
 function showPro3(di) {
     for (const da of di) {
         rooot.innerHTML += `<div class="card">
-    <img class="imgPro" src="${da.image}">
+    <img onclick="getProById(${da.id})" class="imgPro" src="${da.image}">
     <h2 class="name">${da.title}</h2>
     <p class="card-cate">${da.category}</p>
     <p class="card-price">${da.price}$</p>
@@ -104,17 +108,15 @@ async function getPro4() {
 }
 getPro4()
 
-
-
 function showPro4(di) {
     rot.innerHTML = ""
     h3.innerHTML = ""
     for (const da of di) {
         h3.innerHTML = `${da.category}`
         rot.innerHTML += `
-        <div class="card">
+        <div class="card" >
             <div class="test"> 
-                <img class="imgPro" src="${da.image}" />
+                <img onclick="getProById(${da.id})" class="imgPro" src="${da.image}" />
                 <h2 class="name">${da.title}</h2>
                 <p class="card-cate">${da.category}</p>
             </div>
@@ -123,4 +125,30 @@ function showPro4(di) {
     </div>
     `
     }
+}
+
+async function getProById(id) {
+    const res = await fetch(url+"/"+id)
+    const data = await res.json()
+    console.log(data)
+    showSinglPro(data)
+}
+
+function showSinglPro(dd){
+    no1.style.display="none"
+    no2.style.display="none"
+    no3.style.display="none"
+    sale.innerHTML=`<div class="blok">
+    <div class="blok1">
+    <img id="sin-img" src="${dd.image}">
+    </div>
+    <div>
+     <h2 id="sin-h2">${dd.title}</h2>
+      <h3 id="sin-h3">${dd.price}$</h3>
+      <p id="sin-p">${dd.description}</p>
+      <button>Add to cart</button>
+        <button id="sin-btn">Add to favorites</button>
+    </div>
+    </div>
+    `
 }
